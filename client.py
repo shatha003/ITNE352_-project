d197882c36cb208266a4cdce06313e0d75974834
Bayan Isa <xbayaneh3@gmail.com>
2024-11-30 15:51:32 +0300

    initialize the client , and connect it to the server

--- client.py
+++ client.py
@@ -6,3 +6,12 @@ from rich.console import Console
 class NewsClient:
     def __init__(self, host, port):
         #Initialize the client, connect to the server and set up the necessary variables.
+        self.host = host
+        self.port = port
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.host, self.port))
+        self.client_name = input("Enter your name: ")
+        self.client_socket.sendall(self.client_name.encode())
+        self.request_types = ["Search Headlines", "List Sources", "Quit"]
+
+        
