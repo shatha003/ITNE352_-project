39c46c9b7260a6843606152c8058d14c3d7fb18f
Bayan Isa <xbayaneh3@gmail.com>
2024-11-30 15:49:06 +0300

    add client class

--- client.py
+++ client.py
@@ -1,1 +1,8 @@
 import socket
+import json
+import requests
+from rich.console import Console
+
+class NewsClient:
+    def __init__(self, host, port):
+        #Initialize the client, connect to the server and set up the necessary variables.
