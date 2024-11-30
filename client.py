4b1836f22178c726e14406fd55ac26ecf19f25d1
Bayan Isa <xbayaneh3@gmail.com>
2024-11-30 15:53:26 +0300

    loop to handle user input

--- client.py
+++ client.py
@@ -13,5 +13,27 @@ class NewsClient:
         self.client_name = input("Enter your name: ")
         self.client_socket.sendall(self.client_name.encode())
         self.request_types = ["Search Headlines", "List Sources", "Quit"]
+    
+    def run(self):
+        #run the client loop and handle user input. 
+        while True:
+            try:
+                self.display_request_types()
+                request_type_num = int(input("Enter request type (1-3): "))
 
+                if 1 <= request_type_num <= len(self.request_types):
+                    request_type = self.request_types[request_type_num - 1]
+
+                    if request_type == "Search Headlines":
+                        self.search_headlines()
+                    elif request_type == "List Sources":
+                        self.list_sources()
+                    elif request_type == "Quit":
+                        print("See you, Goodbye!")
+                        break
+                else:
+                    print("Invalid request type.")
+            except Exception as e:
+                print(f"An error occurred: {str(e)}")
         
+
